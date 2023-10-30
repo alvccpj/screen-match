5d87fd2e57e4d0060b40ca41f11206510a7aab52
alvccpj <dev.alvarojordao@gmail.com>
2023-10-30 15:22:51 -0300

    add TitudoOmdb

--- src/br/com/alura/screenmatch/main/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/main/PrincipalComBusca.java
@@ -1,13 +1,16 @@
 package br.com.alura.screenmatch.main;
 
 
+import br.com.alura.screenmatch.modelos.Titulo;
 import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
@@ -15,8 +18,9 @@ public class PrincipalComBusca {
         Scanner in = new Scanner(System.in);
         System.out.print("Informe um filme para busca: ");
         String busca = in.nextLine();
+        String buscaCodificada = URLEncoder.encode(busca, StandardCharsets.UTF_8.toString());
 
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=707c3054";
+        String endereco = "https://www.omdbapi.com/?t=" + buscaCodificada + "&apikey=707c3054";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -24,10 +28,14 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
 
 
         Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
     }
 }
 
